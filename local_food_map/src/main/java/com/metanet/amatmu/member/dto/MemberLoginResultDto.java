ef177aedf42288dfb15b61d4044b0c6fa23a59dd
DESKTOP-UA2URNF\KOSA <yjjjwww@naver.com>
2024-01-12 15:19:33 +0900

    git commit -m "Feat : Member 로그인 결과 프론트에 맞게 변경"

---
+++ local_food_map/src/main/java/com/metanet/amatmu/member/dto/MemberLoginResultDto.java
@@ -0,0 +1,15 @@
+package com.metanet.amatmu.member.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class MemberLoginResultDto {
+	private long userId;
+	private String userEmail;
+	private String token;
+	private String userProfileImg;
+}
